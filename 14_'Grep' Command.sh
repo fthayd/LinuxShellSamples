aa1760c3bcf5b54cbc7addc363f62f0e50a3da58
Fatih Aydın <fthayd86@gmail.com>
2019-09-11 09:40:57 +0300

    seventh commit

--- 14_'Grep' Command.sh
+++ 14_'Grep' Command.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 grep -iw -e"the" -e"that" -e"then" -e"those"	# (i)ignore case distinctions, (w)search word and with (e)pattern.
+grep -P '([0-9]) *\1'				# more consecutive occurrences of the same digit
